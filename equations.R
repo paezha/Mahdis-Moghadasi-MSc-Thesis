bf846ba47fdfcd3849dedcff02af232e697891da
Antonio Paez <paezha@gmail.com>
2024-04-09 22:43:06 -0400

    Save equation pieces that were problematic.

---
+++ equations.R
@@ -0,0 +1,84 @@
+l.66
+
+<!--
+  \begin{cases}
+1 & \text{if } c^p_{ij} \le \gamma^p \\
+0 & \text{otherwise}
+\end{cases}
+-->
+
+l.124
+
+<!--
+  \begin{cases}
+1 & \text{if } c_{ij} \le \gamma _i \\
+0 & \text{otherwise}
+\end{cases}
+-->
+
+l.151
+
+<!--
+  \begin{cases}
+1 & \text{if } d_{ij} = \min_{j \in L^p} d_{ij} \\
+0 & \text{otherwise}
+\end{cases}
+-->
+
+  Ch3
+
+l.104
+<!--
+  \begin{cases}
+1 & \text{if } c_{ij} \le x \\
+0 & \text{if } c_{ij} > x
+\end{cases}
+-->
+
+  l.123
+<!--
+  \begin{cases}
+1 & \text{if } t_{ij} \le 1 \\
+t_{ij}^{-\beta} & \text{otherwise}
+\end{cases}
+-->
+
+  l.168
+<!--
+  \begin{cases}
+1 & \text{if } t_{ij} \le t \\
+0 & \text{otherwise}
+\end{cases}
+-->
+
+  l.179
+<!--
+  \begin{cases}
+1 - \frac{t_{ij}}{\bar{t}} & \text{if } t_{ij} \le \bar{t} \\
+0 & \text{otherwise}
+\end{cases}
+-->
+
+  l.190
+<!--
+  \begin{cases}
+\frac{1}{\sigma^\alpha\Gamma(\alpha)} c_{ij}^{\alpha-1} \cdot e^{-\frac{c_{ij}}{\sigma}} & \text{if } 0 \leq c_{ij} < \infty  \text{ and } \alpha, \sigma > 0 \\
+0 & \text{otherwise}
+\end{cases}
+-->
+
+  l.216
+<!--
+  \begin{cases}
+\frac{1}{B(\alpha, \beta)} c_{ij}^{\alpha - 1}(1 - c_{ij})^{\beta - 1} & \text{if } 0 \leq c_{ij} < \infty \\
+0 & \text{otherwise}
+\end{cases}
+-->
+
+  l.237
+<!--
+  \begin{cases}
+\left( \frac{\alpha}{\theta}\right) \left( \frac{c_{ij}}{\theta}\right)^{\alpha - 1} e^{-\left(\frac{c_{ij}}{\theta}\right)^\alpha} & \text{if } 0 \leq c_{ij} \\
+0 & \text{otherwise}
+\end{cases}
+-->
